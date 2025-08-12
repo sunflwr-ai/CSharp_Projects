6b17abb30efe7d319792ed279a0c193e1c3f621c
sunflwr-ai <sunflower69am@gmail.com>
2025-08-12 01:00:04 -0700

    Your commit message here

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba6236bf64b045874756f5485196ff885356888")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f971772e6a0cd92d1f892a1462728be690eed19")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
